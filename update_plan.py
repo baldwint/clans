37374752ee97a57d581d7d92228f4199a22b8cc6
Thomas Baldwin <tbaldwin@uoregon.edu>
2012-06-02 13:38:19 -0700

    Repairing borked repo
    I just went back in time to remove passwords from older commits. I
    didn't do it right, and ended up completely messing the present version.
    
    I restored from a backup, so all should be good. However, versions of
    the script prior to this will be COMPLETELY non-functional.

--- update_plan.py
+++ update_plan.py
@@ -7,127 +7,220 @@ Edit your plan in $EDITOR.
 import urllib2
 from urllib import urlencode
 import cookielib
+import os
+import tempfile
+import subprocess
+from BeautifulSoup import BeautifulSoup
+import hashlib
 
-# params
+# configuration
 loginurl = "http://www.grinnellplans.com/index.php"
 editurl = "http://www.grinnellplans.com/edit.php"
-bakfile = 'plan.orig.txt'
-writefile = 'update.txt'
 
-login_info = {
-    'username': 'baldwint',
-    'password': 'not_my_password',
-#    'username': '2008alums',
-#    'password': 'alum_password',
-    'submit': 'Login'
-}
-
-# make cookie jar
-cj = cookielib.LWPCookieJar()
-
-# install it into urllib2
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
+class PlansError(Exception):
+    """ Raise this when something goes wrong. """
+    pass
 
 # -------------------------------------------
 #              PLANS SCRAPEY-I
 # get it? like "api" except... oh, never mind
 # -------------------------------------------
 
-def plans_login(username, password):
-    """ log into plans """
-    login_info = {'username': username,
-                  'password': password,
-                    'submit': 'Login' }
-    req = urllib2.Request(loginurl)
-    handle = urllib2.urlopen(req, urlencode(login_info))
+class PlansConnection(object):
+    """
+    Encapsulates an active login to plans.
 
-#plans_login('baldwint', 'not_my_password')
-plans_login('2008alums', 'alum_password')
+    """
 
-def get_edit_text():
-    """ retrieve contents of the edit plan field, plus md5 """
-    # grab edit page
-    req = urllib2.Request(editurl)
-    handle = urllib2.urlopen(req)
-    # parse out existing plan
-    soup = BeautifulSoup(handle.read())
-    plan = soup.find('textarea')
-    if plan is None:
-        raise PlansError("Couldn't get edit text, are we logged in?")
-    if plus_hash:
-        # parse out plan md5
-        md5tag = soup.find('input', {'name': 'edit_text_md5'})
-        md5 = md5tag.attrMap['value']
-        return plan.text, md5
+    def __init__(self, cookiejar=None):
+        """
+        Create a new plans connection.
+
+        Optionally, provide a cookiejar to store credentials in.
+
+        """
+        if cookiejar is None:
+            cookiejar = cookielib.LWPCookieJar()
+        proc = urllib2.HTTPCookieProcessor(cookiejar)
+        self.opener = urllib2.build_opener(proc)
+
+    def plans_login(self, username, password):
+        """
+        Log into plans.
+        
+        """
+        # log into plans
+        login_info = {'username': username,
+                      'password': password,
+                        'submit': 'Login' }
+        req = urllib2.Request(loginurl)
+        handle = self.opener.open(req, urlencode(login_info))
+        # verify login by checking that body id="planspage_home"
+        html = handle.read()
+        soup = BeautifulSoup(html)
+        homepage = soup.find('body', {'id': 'planspage_home'})
+        if homepage is None:
+            raise PlansError('Could not log in as [%s].' % username)
+
+    def get_edit_text(self, plus_hash=False):
+        """
+        Retrieve contents of the edit plan field.
+
+        Optionally, simultaneously retrieve the md5 hash of
+        the edit text, as computed on the server side.
+
+        """
+        # grab edit page
+        req = urllib2.Request(editurl)
+        handle = self.opener.open(req)
+        # parse out existing plan
+        soup = BeautifulSoup(handle.read())
+        plan = soup.find('textarea')
+        if plan is None:
+            raise PlansError("Couldn't get edit text, are we logged in?")
+        if plus_hash:
+            # parse out plan md5
+            md5tag = soup.find('input', {'name': 'edit_text_md5'})
+            md5 = md5tag.attrMap['value']
+            return plan.text, md5
+        else:
+            return plan.text
+
+    def set_edit_text(self, newtext, md5):
+        """
+        Update plan with new content.
+
+        To prevent errors, the server does a hash check on the existing
+        plan before replacing it with the new one. We provide an
+        md5 sum to confirm that yes, we really want to update the plan.
+
+        Prints info message to stdout.
+        
+        """
+        edit_info = {         'plan': newtext,
+                     'edit_text_md5': md5,
+                            'submit': 'Change Plan' }
+        req = urllib2.Request(editurl)
+        handle = self.opener.open(req, urlencode(edit_info))
+        soup = BeautifulSoup(handle.read())
+        info = soup.find('div', {'class': 'infomessage'})
+        print info
+
+# ----------
+# UI HELPERS
+# ----------
+
+def edit(text, **kwargs):
+    """
+    Open some text for editing by the user.
+
+    Keyword arguments are passed to the tempfile constructor.
+
+    """
+    if 'text' not in kwargs:
+        kwargs['text'] = True
+
+    # I cribbed this from the mercurial source code, in ui.py.
+    fd, name = tempfile.mkstemp(**kwargs)
+    try:
+        # populate the temp file with original text.
+        f = os.fdopen(fd, 'w')
+        f.write(text)
+        f.close()
+
+        # open in $EDITOR (default to pico)
+        editor = os.environ.get('EDITOR', 'pico')
+        subprocess.call([editor, name])
+
+        # retrieve edited text
+        f = open(name)
+        t = f.read()
+        f.close()
+    finally:
+        os.unlink(name)
+
+    return t
+
+
+
+if __name__ == '__main__':
+    import ConfigParser
+    from optparse import OptionParser
+    import getpass
+
+    class PlansConfigParser(ConfigParser.ConfigParser):
+        """
+        Customized ConfigParser.
+
+        Better handling of missing sections.
+
+        """
+        def get(self, section, option):
+            try:
+                return ConfigParser.ConfigParser.get(self, section, option)
+            except ConfigParser.NoSectionError:
+                return ConfigParser.ConfigParser.get(self, 'DEFAULT', option)
+
+    defaults = {'username': ''}
+
+    config = PlansConfigParser(defaults)
+    config.read(['config.ini',])
+
+    parser = OptionParser(description=__doc__)
+    parser.add_option('-u', '--username', dest='username',
+              help='GrinnellPlans username, no brackets.')
+    parser.add_option('-p', '--password', dest='password',
+              help='GrinnellPlans password. Omit for secure entry.')
+    opts, args = parser.parse_args()
+
+    username = opts.username or config.get('login', 'username')
+
+    cj = cookielib.LWPCookieJar('%s.cookie' % username)
+    try:
+        cj.load() # this will fail with IOError if it does not exist
+                  #TODO: what if it exists, but is expired?
+    except IOError:
+        # no cookie saved for this user; log in.
+        if opts.password is None:
+            # prompt for password if necessary
+            opts.password = getpass.getpass("[%s]'s password: " % username)
+            if '\x03' in opts.password:
+                # http://bugs.python.org/issue11236 (2.6 only)
+                raise PlansError('aborted by user')
+        pc = PlansConnection(cj)
+        pc.plans_login(username, opts.password)
     else:
-        return plan.text
+        pc = PlansConnection(cj)
 
-#plans_login('baldwint', 'not_my_password')
-#cj = plans_login('2008alums', 'wrong_password')
-cj = plans_login('2008alums', 'alum_password')
-plan_text, md5 = get_edit_text(plus_hash=True)
+    plan_text, md5 = pc.get_edit_text(plus_hash=True)
 
-print md5
-print hashlib.md5(plan_text.encode('utf8')).hexdigest()
-#print plan_text.encode('utf8')
+    #TODO: get these to match somehow
+    #print md5
+    #print hashlib.md5(plan_text.encode('utf8')).hexdigest()
 
-bakfile = 'plan.bak.txt'
-editfile = 'plan.edited.txt'
-writefile = 'fresh.txt'
+    bakfile = 'plan.bak.txt'
+    editfile = 'plan.edited.txt'
 
-# save existing plan
-fp = open(bakfile, 'w')
-fp.write(plan_text.encode('utf8'))
-fp.close
+    # save existing plan
+    fp = open(bakfile, 'w')
+    fp.write(plan_text.encode('utf8'))
+    fp.close()
 
-# open plan for editing.
-# cribbed from the mercurial source code, in ui.py.
-fd, name = tempfile.mkstemp(suffix='.plan', text=True)
-
-try:
-    # populate the temp file with original text.
-    f = os.fdopen(fd, 'w')
-    f.write(plan_text.encode('utf8'))
-    f.close()
-
-    # open in $EDITOR
-    editor = os.environ.get('EDITOR', 'pico')
-    subprocess.call([editor, name])
-
-    # retrieve edited text
-    f = open(name)
-    t = f.read()
-    f.close()
+    # open for external editing
+    edited = edit(plan_text.encode('utf8'), suffix='.plan')
 
     # ok, now save edited file
     fp = open(editfile, 'w')
-    fp.write(t)
+    fp.write(edited)
     fp.close()
 
-finally:
-    os.unlink(name)
+    # do the plan update!
+    if edited != plan_text.encode('utf8'):
+        pc.set_edit_text(edited, md5)
+    else:
+        print 'plan unchanged, aborting'
 
-# load up new plan
-fp = open(writefile, 'r')
-new_plan = fp.read()
-fp.close()
-
-def update_plan(newtext, md5):
-    edit_info = {         'plan': newtext,
-                 'edit_text_md5': md5,
-                        'submit': 'Change Plan' }
-    req = urllib2.Request(editurl)
-    handle = urllib2.urlopen(req, urlencode(edit_info))
-    soup = BeautifulSoup(handle.read())
-    info = soup.find('div', {'class': 'infomessage'})
-    print info
-
-# do the plan update!
-update_plan(edited, md5)
-
-#save cookie
-#cj_filename = "plans.python.cookie"
-#cj.save(cj_filename)
+    # save cookie
+    cj.save()
 
 
