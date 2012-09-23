6b079a8c8022275d19d402b72b6b17552628db8b
Thomas Baldwin <tbaldwin@uoregon.edu>
2012-09-23 15:07:03 -0700

    Implemented subcommand structure
    Split main function in two, it now calls 'edit' function to do plan
    editing
    
    CLI is now different, 'uplan' behaves like 'clans' - need to do 'uplan
    edit' to call edit function. Will rename soon.

--- update_plan.py
+++ update_plan.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Edit your plan in $EDITOR."""
+"""Command-line Plans."""
 
 import cookielib
 import os
@@ -43,85 +43,17 @@ def external_editor(text, **kwargs):
 
     return t
 
+# -----------
+# SUBCOMMANDS
+# -----------
 
-def main():
-    import ConfigParser
-    from argparse import ArgumentParser
-    import getpass as getpass_mod
+# these are functions that take three agruments:
+#  - a PlansConnection instance
+#  - a namespace of parsed arguments
+#  - a configfile instance.
 
-    def getpass(*args, **kwargs):
-        password = getpass_mod.getpass(*args, **kwargs)
-        if '\x03' in password:
-            # http://bugs.python.org/issue11236 (2.6 only)
-            raise KeyboardInterrupt('aborted by user')
-        return password
-
-    # set config file defaults
-    config = ConfigParser.ConfigParser()
-    config.add_section('login')
-    config.set('login', 'username', '')
-    config.set('login', 'url', 'http://www.grinnellplans.com')
-
-    # create config directory if it doesn't exist
-    config_dir = os.path.join(os.environ['HOME'], '.update_plan')
-    try:
-        # 0700 for secure-ish cookie storage.
-        os.mkdir(config_dir, 0700)
-    except OSError:
-        pass # already exists
-
-    # read user's config file, if present
-    config.read(os.path.join(config_dir, 'update_plan.cfg'))
-
-    # define command line arguments
-    parser = ArgumentParser(description=__doc__)
-    parser.add_argument('-u', '--username', dest='username', default='',
-              help='GrinnellPlans username, no brackets.')
-    parser.add_argument('-p', '--password', dest='password', default='',
-              help='GrinnellPlans password. Omit for secure entry.')
-    parser.add_argument('-b', '--backup', dest='backup_file',
-                        nargs='?', default=False, metavar='FILE',
-              help="""Backup existing plan to file before editing.
-                        To print to stdout, omit filename.""")
-    parser.add_argument('-s', '--save', dest='save_edit',
-                        default=False, metavar='FILE',
-              help='Save a local copy of edited plan before submitting.')
-    parser.add_argument('--skip-update', dest='skip_update',
-                        action='store_true', default=False,
-              help="Don't update the plan or open it for editing.")
-    parser.add_argument('--pretend', dest='pretend',
-                        action='store_true', default=False,
-              help="Open plan for editing, but don't actually do the update.")
-    parser.add_argument('--logout', dest='logout',
-                        action='store_true', default=False,
-              help='Log out after editing.')
-
-    # get command line arguments
-    args = parser.parse_args()
-
-    # let command line args override equivalent config file settings
-    username = args.username or config.get('login', 'username')
-
-    cj = cookielib.LWPCookieJar(
-        os.path.join(config_dir, '%s.cookie' % username))
-
-    try:
-        cj.load() # this will fail with IOError if it does not exist
-    except IOError:
-        pass      # no cookie saved for this user
-
-    pc = PlansConnection(cj, base_url = config.get('login', 'url'))
-
-    if pc.plans_login():
-        pass # we're still logged in
-    else:
-        # we're not logged in, prompt for password if necessary
-        password = args.password or getpass("[%s]'s password: " % username)
-        success = pc.plans_login(username, password)
-        if not success:
-            print >> sys.stderr, 'Failed to log in as [%s].' % username
-            sys.exit(1)
-
+def edit(pc, args, config):
+    """ plan-editing command """
     plan_text, md5 = pc.get_edit_text(plus_hash=True)
 
     #TODO: get these to match somehow
@@ -162,6 +94,103 @@ def main():
         # do the plan update!
         info = pc.set_edit_text(edited, md5)
         print >> sys.stderr, info
+
+def main():
+    import ConfigParser
+    from argparse import ArgumentParser
+    import getpass as getpass_mod
+
+    def getpass(*args, **kwargs):
+        password = getpass_mod.getpass(*args, **kwargs)
+        if '\x03' in password:
+            # http://bugs.python.org/issue11236 (2.6 only)
+            raise KeyboardInterrupt('aborted by user')
+        return password
+
+    # set config file defaults
+    config = ConfigParser.ConfigParser()
+    config.add_section('login')
+    config.set('login', 'username', '')
+    config.set('login', 'url', 'http://www.grinnellplans.com')
+
+    # create config directory if it doesn't exist
+    config_dir = os.path.join(os.environ['HOME'], '.update_plan')
+    try:
+        # 0700 for secure-ish cookie storage.
+        os.mkdir(config_dir, 0700)
+    except OSError:
+        pass # already exists
+
+    # read user's config file, if present
+    config.read(os.path.join(config_dir, 'update_plan.cfg'))
+
+    # define command line arguments
+
+    # globals: options/arguments inherited by all parsers, including root
+    global_parser = ArgumentParser(add_help=False)
+
+    global_parser.add_argument('-u', '--username', dest='username', default='',
+              help='GrinnellPlans username, no brackets.')
+    global_parser.add_argument('-p', '--password', dest='password', default='',
+              help='GrinnellPlans password. Omit for secure entry.')
+    global_parser.add_argument('--logout', dest='logout',
+                        action='store_true', default=False,
+                      help='Log out before quitting.')
+
+    # main parser: has subcommands for everything
+    parser = ArgumentParser(description=__doc__, parents=[global_parser])
+
+    subparsers = parser.add_subparsers(title = "possible commands")
+
+    # edit parser: options/arguments for editing plans
+    edit_parser = subparsers.add_parser('edit', parents=[global_parser],
+                        description='Opens your plan for editing in a text editor.',
+                            help='Edit your plan in $EDITOR.')
+    edit_parser.set_defaults(func=edit)
+    edit_parser.add_argument('-b', '--backup', dest='backup_file',
+                        nargs='?', default=False, metavar='FILE',
+              help="""Backup existing plan to file before editing.
+                        To print to stdout, omit filename.""")
+    edit_parser.add_argument('-s', '--save', dest='save_edit',
+                        default=False, metavar='FILE',
+              help='Save a local copy of edited plan before submitting.')
+    edit_parser.add_argument('--skip-update', dest='skip_update',
+                        action='store_true', default=False,
+              help="Don't update the plan or open it for editing.")
+    edit_parser.add_argument('--pretend', dest='pretend',
+                        action='store_true', default=False,
+              help="Open plan for editing, but don't actually do the update.")
+
+    # plugins down here (later)
+
+    # get command line arguments
+    args = parser.parse_args()
+
+    # let command line args override equivalent config file settings
+    username = args.username or config.get('login', 'username')
+
+    cj = cookielib.LWPCookieJar(
+        os.path.join(config_dir, '%s.cookie' % username))
+
+    try:
+        cj.load() # this will fail with IOError if it does not exist
+    except IOError:
+        pass      # no cookie saved for this user
+
+    pc = PlansConnection(cj, base_url = config.get('login', 'url'))
+
+    if pc.plans_login():
+        pass # we're still logged in
+    else:
+        # we're not logged in, prompt for password if necessary
+        password = args.password or getpass("[%s]'s password: " % username)
+        success = pc.plans_login(username, password)
+        if not success:
+            print >> sys.stderr, 'Failed to log in as [%s].' % username
+            sys.exit(1)
+
+    # pass execution to the subcommand
+    args.func(pc, args, config)
 
     if args.logout:
         os.unlink(cj.filename)
