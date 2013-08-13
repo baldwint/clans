7d0d9b92044cb10af22a5be7da5a9f1009e80550
Thomas Baldwin <tbaldwin@uoregon.edu>
2013-08-12 22:35:54 -0700

    Refactor fmt tests
    Test classes share test phrases now, this makes sense

--- tests/test_fmt.py
+++ tests/test_fmt.py
@@ -13,7 +13,36 @@ else:
 import clans.fmt
 import sys
 
-#TODO: TestRaw, TestColor
+TEST_DATA = {
+        'test_format_plan': {
+                'lastupdated': 'Mon August 5th 2013, 1:22 PM',
+                'lastlogin': 'Wed August 7th 2013, 3:42 AM',
+                'username': 'username',
+                'planname': 'clever catchphrase',
+                'plan': 'this is my plan with the html filtered out\n'
+                },
+        'test_br_stripping': "one<br>two<br/>three<br />four",
+        'test_html_escapes': "I develop in a &quot;quick &amp; dirty&quot; &lt;style&gt;",
+        'test_tag_stripping': "This is <b>bold</b> and <i>italic</i>",
+        'test_link_formatting': '<a href="http://www.facebook.com/" class="onplan">my favorite website</a>',
+        'test_love_formatting': '[<a href="read.php?searchname=gorp" class="planlove">GORP</a>]',
+        'test_love_formatting': '[<a href="read.php?searchname=gorp" class="planlove">GORP</a>]',
+        'test_psub_formatting': '<p class="sub">we all live in a yellow</p>',
+        'test_hr_formatting': """I need a clean
+<hr>break""",
+        'test_print_list': ['one', 'two', 'three', 'four'],
+        'test_print_autoread': {
+                'Level 1': ['bff', 'interesting', 'funny', 'gorp'],
+                'Level 2': ['roommate', 'rando'],
+                'Level 3': ['meh',],
+                },
+        'test_print_search_results': [
+                ('plan1', 1, ['snip one <b>term</b> context',]),
+                ('plan2', 2, ['snip one <b>term</b> context',
+                              'snip two <b>term</b> context']),
+                ('plan3', 2, ['snip <b>term</b> twice <b>term</b> twice',])
+                ],
+        }
 
 class TestText(unittest.TestCase):
 
@@ -23,14 +52,8 @@ class TestText(unittest.TestCase):
     # plan format test
 
     def test_format_plan(self):
-        headers = {
-                'lastupdated': 'Mon August 5th 2013, 1:22 PM',
-                'lastlogin': 'Wed August 7th 2013, 3:42 AM',
-                'username': 'username',
-                'planname': 'clever catchphrase',
-                }
-        plan = 'this is my plan with the html filtered out\n'
-        text = self.fmt.format_plan(plan = plan, **headers)
+        data = TEST_DATA['test_format_plan']
+        text = self.fmt.format_plan(**data)
         expect = """\
 Username: username
 Last Updated: Mon August 5th 2013, 1:22 PM
@@ -45,44 +68,43 @@ this is my plan with the html filtered out
     # filter_html tests
 
     def test_br_stripping(self):
-        html = "one<br>two<br/>three<br />four"
+        html = TEST_DATA['test_br_stripping']
         text = self.fmt.filter_html(html)
         expect = "one\ntwo\nthree\nfour"
         self.assertEqual(expect, text)
 
     def test_html_escapes(self):
-        html = "I develop in a &quot;quick &amp; dirty&quot; &lt;style&gt;"
+        html = TEST_DATA['test_html_escapes']
         text = self.fmt.filter_html(html)
         expect = 'I develop in a "quick & dirty" <style>'
         self.assertEqual(expect, text)
 
     def test_tag_stripping(self):
-        html = "This is <b>bold</b> and <i>italic</i>"
+        html = TEST_DATA['test_tag_stripping']
         expect = "This is bold and italic"
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_link_formatting(self):
-        html = '<a href="http://www.facebook.com/" class="onplan">my favorite website</a>'
+        html = TEST_DATA['test_link_formatting']
         expect = '[http://www.facebook.com/|my favorite website]'
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_love_formatting(self):
-        html = '[<a href="read.php?searchname=gorp" class="planlove">GORP</a>]'
+        html = TEST_DATA['test_love_formatting']
         expect = '[GORP]'
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_psub_formatting(self):
-        html = '<p class="sub">we all live in a yellow</p>'
+        html = TEST_DATA['test_psub_formatting']
         expect = 'we all live in a yellow'
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_hr_formatting(self):
-        html = """I need a clean
-<hr>break"""
+        html = TEST_DATA['test_hr_formatting']
         expect = """I need a clean
 
 ======================================================================
@@ -93,7 +115,8 @@ break"""
     # other tests
 
     def test_print_list(self):
-        self.fmt.print_list(['one', 'two', 'three', 'four'])
+        lst = TEST_DATA['test_print_list']
+        self.fmt.print_list(lst)
         output = sys.stdout.getvalue()
         expect = u"""\
  - one
@@ -104,12 +127,7 @@ break"""
         self.assertEqual(expect, output)
 
     def test_print_search_results(self):
-        results = [
-                ('plan1', 1, ['snip one <b>term</b> context',]),
-                ('plan2', 2, ['snip one <b>term</b> context',
-                              'snip two <b>term</b> context']),
-                ('plan3', 2, ['snip <b>term</b> twice <b>term</b> twice',])
-                ]
+        results = TEST_DATA['test_print_search_results']
         self.fmt.print_search_results(results)
         output = sys.stdout.getvalue()
         expect = u"""\
@@ -130,11 +148,7 @@ break"""
         self.assertEqual(expect, output)
 
     def test_print_autoread(self):
-        autoread = {
-                'Level 1': ['bff', 'interesting', 'funny', 'gorp'],
-                'Level 2': ['roommate', 'rando'],
-                'Level 3': ['meh',],
-                }
+        autoread = TEST_DATA['test_print_autoread']
         self.fmt.print_autoread(autoread)
         output = sys.stdout.getvalue()
         expect = u"""\
@@ -162,14 +176,8 @@ class TestColor(TestText):
     # plan format test
 
     def test_format_plan(self):
-        headers = {
-                'lastupdated': 'Mon August 5th 2013, 1:22 PM',
-                'lastlogin': 'Wed August 7th 2013, 3:42 AM',
-                'username': 'username',
-                'planname': 'clever catchphrase',
-                }
-        plan = 'this is my plan with the html filtered out\n'
-        text = self.fmt.format_plan(plan = plan, **headers)
+        data = TEST_DATA['test_format_plan']
+        text = self.fmt.format_plan(**data)
         expect = """\
 %sUsername%s: username
 %sLast Updated%s: Mon August 5th 2013, 1:22 PM
@@ -184,28 +192,27 @@ this is my plan with the html filtered out
     # filter_html tests
 
     def test_tag_stripping(self):
-        html = "This is <b>bold</b> and <i>italic</i>"
+        html = TEST_DATA['test_tag_stripping']
         expect = "This is \x1b[1mbold\x1b[22m and \x1b[2mitalic\x1b[22m"
         # italic is actually 'dim', bold 'bright'
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_link_formatting(self):
-        html = '<a href="http://www.facebook.com/" class="onplan">my favorite website</a>'
+        html = TEST_DATA['test_link_formatting']
         expect = '[\x1b[32mhttp://www.facebook.com/\x1b[39m|\x1b[35mmy favorite website\x1b[39m]'
         # green for the link, magenta for the link text
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_love_formatting(self):
-        html = '[<a href="read.php?searchname=gorp" class="planlove">GORP</a>]'
+        html = TEST_DATA['test_love_formatting']
         expect = '[\x1b[1m\x1b[34mGORP\x1b[22m\x1b[39m]' # blue and bold
         text = self.fmt.filter_html(html)
         self.assertEqual(expect, text)
 
     def test_hr_formatting(self):
-        html = """I need a clean
-<hr>break"""
+        html = TEST_DATA['test_hr_formatting']
         expect = """I need a clean
 
 \x1b[31m======================================================================\x1b[39m
@@ -217,12 +224,7 @@ break"""
     # other tests
 
     def test_print_search_results(self):
-        results = [
-                ('plan1', 1, ['snip one <b>term</b> context',]),
-                ('plan2', 2, ['snip one <b>term</b> context',
-                              'snip two <b>term</b> context']),
-                ('plan3', 2, ['snip <b>term</b> twice <b>term</b> twice',])
-                ]
+        results = TEST_DATA['test_print_search_results']
         self.fmt.print_search_results(results)
         output = sys.stdout.getvalue()
         expect = u"""\
