f606c5d881b560bd559eb88da92c3b88d1f7787b
Thomas Baldwin <tbaldwin@uoregon.edu>
2014-01-24 15:29:01 -0800

    added convert_endings convenience function

--- clans/util.py
+++ clans/util.py
@@ -1,6 +1,17 @@
 from hashlib import md5
+import re
 
 
 def plans_md5(plan):
     """compute the md5 sum of a plan, for verification"""
     return md5(plan.encode('utf8')).hexdigest()
+
+
+def convert_endings(string, mode):
+    mode = mode.upper()
+    if mode == 'CRLF':
+        return re.sub("\r(?!\n)|(?<!\r)\n", "\r\n", string)
+    elif mode == 'LF':
+        return string.replace('\r\n', '\n').replace('\r', '\n')
+    elif mode == 'CR':
+        return string.replace('\r\n', '\r').replace('\n', '\r')
