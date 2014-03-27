db36ba9afa5b9a7e73e38b23de8ff1c19d08dff1
Thomas Baldwin <tbaldwin@uoregon.edu>
2014-03-27 10:13:27 -0700

    Switch to new RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -93,9 +93,12 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
-#RTD theme
-html_theme = "armstrong_sphinx"
-html_theme_path = ["_themes", ]
+# are we building on readthedocs.org?
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = ["_themes/sphinx_rtd_theme", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
